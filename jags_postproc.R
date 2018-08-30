4d8daa14333cbd4643e36e2dcaa62e41e8f93fb4
GregGU <ggur1373@gmail.com>
2018-08-30 17:24:30 -0400

    added detailed comments

--- jags_postproc.R
+++ jags_postproc.R
@@ -1,6 +1,7 @@
+# Load Jags data
 load(file.path(workdir, data_dir_jags))
 
-# Transform data to long format
+# Transform data from wide to long, create numeric country column, floor year, create index
 proportions_jags <- list()
 prop_names <- c("Total","Modern","Traditional","Unmet")
 res_country_obj <- res.country$CIprop.Lg.Lcat.qt
@@ -9,17 +10,17 @@ for (proportion in prop_names){
   dls <- list()
 for (country in names(res_country_obj)) {
   dls[[country]] <- res_country_obj[[country]][[proportion]] %>%
-          aperm() %>% #this transposes the data
+          aperm() %>% # aperm transposes the data
           as.data.frame() %>% 
-          dplyr::add_rownames("year_idx") %>%
-          dplyr::mutate(year_idx = floor(as.numeric(year_idx)))
+          dplyr::add_rownames("year_idx") %>% # creates a column based on rownames
+          dplyr::mutate(year_idx = floor(as.numeric(year_idx))) # floor the year
 }
 proportions_jags[[proportion]] <- do.call(rbind, dls) %>% 
   as.data.frame %>%
-  dplyr::add_rownames("country_idx") %>%
-  dplyr::mutate(country = gsub("[^[:alpha:], ]","", country_idx)) %>%
-  dplyr::mutate(country_idx = as.numeric(as.factor(country))) %>%
-  dplyr::mutate(group_1 = 1:length(country_idx)) %>%
+  dplyr::add_rownames("country_idx") %>% # again create column based on rownames
+  dplyr::mutate(country = gsub("[^[:alpha:], ]","", country_idx)) %>% # retain only characters in names
+  dplyr::mutate(country_idx = as.numeric(as.factor(country))) %>% # create numeric country index based on given jags order
+  dplyr::mutate(group_1 = 1:length(country_idx)) %>% # create basic index
   as.data.frame()
 }
 
